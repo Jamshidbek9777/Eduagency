9315d25d2f820181864a04a3f74c10a505d0095c
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-08-27 17:04:36 +0500

    change

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -12,7 +12,7 @@ const useHeaderData = () => {
         { title: getText("headerSwiperTitle1"), description: getText("headerSwiperParagraph1"), headerLink: '/application' },
         { title: getText("headerSwiperTitle2"), description: getText("headerSwiperParagraph2"), headerLink: '/student-transfer' },
         { title: getText("headerSwiperTitle3"), description: getText("headerSwiperParagraph3"), headerLink: '/expert-transfer' },
-        { title: getText("headerSwiperTitle4"), description: getText("headerSwiperParagraph4"), headerLink: "/turkiye-ofisi" }
+        // { title: getText("headerSwiperTitle4"), description: getText("headerSwiperParagraph4"), headerLink: "/turkiye-ofisi" }
       ];
       setHeaderData(data);
     };
