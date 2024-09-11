86a711952d26ab288eb731cf91852fe14dfe45bf
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-09-11 18:00:46 +0500

    add google a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Services from "./components/Services";
 import Getintouch from "./components/Getintouch";
 import Partners from "./components/Partners";
 import ServiceBlog from "./components/ServiceBlog";
+import ReactGA from "react-ga4";
 
 const App = () => {
-  
+  useEffect(() => {
+    ReactGA.initialize("G-KSW67VFGP9");
+
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <Header />
