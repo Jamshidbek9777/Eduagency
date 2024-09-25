4bd23cd86d6b8db988b3c1291a91b4bed93f454f
AbdulazeezwithWerr <abdulazizmamdaliyev16@gmail.com>
2024-09-25 13:05:45 +0500

    change header

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -10,9 +10,9 @@ const useHeaderData = () => {
         const fetchData = () => {
             const data = [
                 { title: getText("headerSwiperTitle2"), description: getText("headerSwiperParagraph2"), headerLink: '/student-transfer', bgImage: "/img/header2.png", mobBgImage: "/img/headerMob2.jpg" },
-                { title: getText("headerSwiperTitle4"), description: getText("headerSwiperParagraph4"), headerLink: "/turkiye-dili", bgImage: "/img/header4.jpg", mobBgImage: "/img/header4.jpg" },
-                { title: getText("headerSwiperTitle1"), description: getText("headerSwiperParagraph1"), headerLink: '/application', bgImage: "/img/header-bg.png", mobBgImage: "/img/7129ee73fb9c15e17860c99924215444.png" },
                 { title: getText("headerSwiperTitle3"), description: getText("headerSwiperParagraph3"), headerLink: '/expert-transfer', bgImage: "/img/header3.png", mobBgImage: "/img/headerMob3.jpg" },
+                { title: getText("headerSwiperTitle1"), description: getText("headerSwiperParagraph1"), headerLink: '/application', bgImage: "/img/header-bg.png", mobBgImage: "/img/7129ee73fb9c15e17860c99924215444.png" },
+                { title: getText("headerSwiperTitle4"), description: getText("headerSwiperParagraph4"), headerLink: "/turkiye-dili", bgImage: "/img/header4.jpg", mobBgImage: "/img/header4.jpg" },
             ];
             setHeaderData(data);
         };
